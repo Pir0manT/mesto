b16df818aeede5c60571c496597af44d88165fa9
Andrey Sergeyev <savilit@yandex.ru>
2022-12-20 05:14:30 +0300

    feat: добавить закрытие popup по esc и клику на overlay

--- scripts/index.js
+++ scripts/index.js
@@ -51,14 +51,23 @@ const formAddElement = modalAddElement.querySelector('.popup__form');
 //            функции               //
 //************************************
 
+// закрытие popup по клавише Esc
+function closePopupByEsc(evt) {
+  if (evt.key === 'Escape') {
+    closeModalWindow(document.querySelector(".popup_opened"));
+  }
+}
+
 // открывает popup
 function openModalWindow(modalWindow) {
   modalWindow.classList.add('popup_opened')
+  document.addEventListener('keydown', closePopupByEsc)
 }
 
 // закрывает popup
 function closeModalWindow(modalWindow) {
   modalWindow.classList.remove('popup_opened')
+  document.removeEventListener('keydown', closePopupByEsc)
 }
 
 // возвращает Node картинки по шаблону из переданного объекта
@@ -119,6 +128,12 @@ formAddElement.addEventListener('submit', (evt) => {
   elementsList.prepend(createElement({name: inputElementName.value, link: inputElementLink.value}));
   formAddElement.reset();
   closeModalWindow(modalAddElement);
+})
+// закрытие popup кликом по overlay
+document.addEventListener('mousedown', evt => {
+  if (evt.target.classList.contains("popup")){
+    closeModalWindow(document.querySelector(".popup_opened"));
+  }
 })
 
 //************************************
