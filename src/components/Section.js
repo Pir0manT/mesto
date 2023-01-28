514373d02c3bf33fb20ac687d951c91a646c249f
Andrey Sergeyev <savilit@yandex.ru>
2023-01-28 23:08:23 +0300

    hotfix: исправления перед первым ревью

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,14 +3,14 @@
 //               элементов          //
 //************************************
 export class Section {
-  constructor({ data, renderer }, containerSelector) {
-    this._items = data
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items
     this._renderer = renderer
     this._container = document.querySelector(containerSelector)
   }
 
   renderItems = () => {
-    this._renderer(this._items,this._container)
+    this._renderer(this._items, this._container)
   }
 
   addItem = (item) => {
